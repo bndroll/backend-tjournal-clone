165d387290971fedfb80392991310bcf4e12a96c
bounderoll <bounderoll.23@gmail.com>
2021-09-21 15:46:38 +0400

    init: initialize CRUD for user auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from './user/user.module'
@@ -8,6 +9,7 @@ import { PostModule } from './post/post.module'
 import { PostEntity } from './post/entities/post.entity'
 import { CommentModule } from './comment/comment.module'
 import { CommentEntity } from './comment/entities/comment.entity'
+import { AuthModule } from './auth/auth.module'
 
 
 @Module({
@@ -24,7 +26,9 @@ import { CommentEntity } from './comment/entities/comment.entity'
         }),
         UserModule,
         PostModule,
-        CommentModule],
+        CommentModule,
+        AuthModule
+    ],
     controllers: [AppController],
     providers: [AppService]
 })
